fb9c490416a45102505d354f7131f943f26640b2
chigley22 <102197101+chigley22@users.noreply.github.com>
2022-09-27 01:10:42 -0600

    portfolio updated with portfolio.json and project cards

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,11 @@ import React from "react";
 import "../assets/styles/Contact.css";
 
 function Contact() {
-  return <div id="contact">Contact</div>;
+  return (
+    <div id="contact">
+      <h1>Contact Form:</h1>
+    </div>
+  );
 }
 
 export default Contact;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,35 @@
 import React from "react";
+import ProjectCards from "./ProjectCard";
+import portfolio from "../portfolio.json";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/styles/Portfolio.css";
 
+function Wrapper(props) {
+  return <div className="wrapper">{props.children}</div>;
+}
+
 function Portfolio() {
-  return <div id="portfolio">Portfolio</div>;
+  return (
+    <section>
+      <div className="project">
+        <h1 className="title">Portfolio:</h1>
+        <hr></hr>
+      </div>
+
+      <Wrapper id="card-data">
+        {portfolio.map((project) => (
+          <ProjectCards
+            key={project.id}
+            image={project.image}
+            name={project.name}
+            github={project.github}
+            deployed={project.deployed}
+            topics={project.topics}
+          />
+        ))}
+      </Wrapper>
+    </section>
+  );
 }
 
 export default Portfolio;
