be7282f8cacf9759b4a3d86154799ef148bf6e64
Colton Higley <cman_hig@hotmail.com>
2022-09-26 19:39:35 -0600

    footer and Header added

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function Footer() {
+    return ( 
+        <div id="Footer">
+            Footer
+        </div>
+     );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function Header() {
+    return ( 
+        <div id="Header">
+            Header
+        </div>
+     );
+}
+
+export default Header;
